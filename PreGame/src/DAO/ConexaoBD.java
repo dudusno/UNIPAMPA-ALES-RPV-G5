c54489787726a3185bc24413011c49268fb8712e
dudusno <huillian15@gmail.com>
2018-03-30 21:59:31 -0300

    principio primeFaces javaweb biblioteca jdbc principio conexao BD

--- PreGame/src/DAO/ConexaoBD.java
+++ PreGame/src/DAO/ConexaoBD.java
@@ -4,10 +4,24 @@
  * and open the template in the editor.
  */
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author unipampa
  */
 public class ConexaoBD {
+
+    public ConexaoBD() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        Connection connection = null;
+        connection = DriverManager.getConnection(
+                "jdbc:postgresql://hostname:RPMal", "postgre", "admin");
+        //connection.close();
+    }
     
+    
+
 }
---
+++ PreGame/src/Model/Mensagem.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author unipampa
+ */
+public class Mensagem 
+{
+    private String conteudo;
+    
+    public String getConteudo (){return this.conteudo;}
+    public void setConteudo (String conteudo){this.conteudo = conteudo;}    
+}
